bc1af40c40d8e8696dfc8716a02cfa25e6863b56
mambetalieva <116174694+mambetalieva@users.noreply.github.com>
2022-12-28 17:58:47 +0600

    Update ViewController.swift

--- layout2hdHome W/ViewController.swift
+++ layout2hdHome W/ViewController.swift
@@ -5,13 +5,37 @@
 //  Created by Каира on 28.12.2022.
 //
 
+//
+//  ViewController.swift
+//  layout2hdHome W
+//
+//  Created by Каира on 28.12.2022.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var Coordination: UIImageView!
+    
+    @IBOutlet weak var Arsenal: UIImageView!
+    
+    
+    @IBOutlet weak var Songs: UIImageView!
+    
+    
+    @IBOutlet weak var Techniques: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Coordination.layer.cornerRadius = 100 / 2
+        Arsenal.layer.cornerRadius = 100 / 2
+        Songs.layer.cornerRadius = 100 / 2
+        Techniques.layer.cornerRadius = 100 / 2
+        
     }
 
 
