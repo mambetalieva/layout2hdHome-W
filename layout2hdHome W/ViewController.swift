19aba9b41aa5c174bb9901436d75dd1b86575976
kayra <mgulkajyr@inbox.ru>
2022-12-28 17:24:33 +0600

    Initial Commit

---
+++ layout2hdHome W/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layout2hdHome W
+//
+//  Created by Каира on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
